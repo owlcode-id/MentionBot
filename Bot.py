a2aa6bb47c0c0f829a44bf5f880ae558b8f2d9ae
owlcode-id <127479298+owlcode-id@users.noreply.github.com>
2023-08-20 20:11:32 +0700

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -121,34 +121,33 @@ async def mentionall(event):
         )
 
 
-    spam_chats.append(chat_id)
-    usrnum = 0
-    usrtxt = ""
-    async for usr in client.iter_participants(chat_id):
-        if not chat_id in spam_chats:
-            break
-        usrnum += 1
-        usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
-        if usrnum == 5:
-            if mode == "text_on_cmd":
-                txt = f"{usrtxt}\n\n{msg}"
-                await client.send_message(chat_id, txt)
-            elif mode == "text_on_reply":
-                await msg.reply(usrtxt)
-            await asyncio.sleep(2)
-            usrnum = 0
-            usrtxt = ""
-    try:
-        spam_chats.remove(chat_id)
-    except:
-        pass
-
+spam_chats.append(chat_id)
+usrnum = 0
+usrtxt = ""
+async for usr in client.iter_participants(chat_id):
+    if not chat_id in spam_chats:
+        break
+    usrnum += 1
+    usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
+    if usrnum == 5:
+        if mode == "text_on_cmd":
+            txt = f"{usrtxt}\n\n{msg}"
+            await client.send_message(chat_id, txt)
+        elif mode == "text_on_reply":
+            await msg.reply(usrtxt)
+        await asyncio.sleep(2)
+        usrnum = 0
+        usrtxt = ""
+try:
+    spam_chats.remove(chat_id)
+except:
+    pass
 
 @client.on(events.NewMessage(pattern="^/admins|/admin|@admin|@admins ?(.*)"))
 async def _(event):
     chat_id = event.chat_id
     if event.is_private:
-        return await event.respond("sᴏʀʀʏ ʏᴏᴜ ᴄᴀɴ ᴍᴇɴᴛɪᴏɴ ᴀᴅᴍɪɴ ᴏɴʟʏ ɪɴ ɢʀᴏᴜᴘ")
+        return await event.respond("ᴍᴀᴀғ, ʜᴀɴʏᴀ ᴀᴅᴍɪɴ ʏᴀɴɢ ʙɪsᴀ ᴍᴇɴɢᴍᴇɴᴛɪᴏɴ ʙᴇʀᴜʙᴀʜᴀɴ ᴅɪ ɢʀᴜᴘ")
 
     is_admin = False
     try:
@@ -161,10 +160,10 @@ async def _(event):
         ):
             is_admin = True
     if not is_admin:
-        return await event.respond("ᴏɴʟʏ ᴀᴅᴍɪɴ ᴄᴀɴ ᴍᴇɴᴛɪᴏɴ ɢʀᴏᴜᴘ ᴀᴅᴍɪɴs")
+        return await event.respond("ʜᴀɴʏᴀ ᴀᴅᴍɪɴ ɢʀᴜᴘ ʙɪsᴀ ᴍᴇɴɢᴍᴇɴᴛɪᴏɴ ᴀᴅᴍɪɴ ɢʀᴜᴘ")
 
     if event.pattern_match.group(1) and event.is_reply:
-        return await event.respond("ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ ᴍᴇɴᴛɪᴏɴ")
+        return await event.respond("ʙᴇʀɪᴋᴀɴʟᴀʜ ᴛᴇᴋs ᴜɴᴛᴜᴋ ᴍᴇɴɢᴍᴇɴᴛɪᴏɴ")
     elif event.pattern_match.group(1):
         mode = "text_on_cmd"
         msg = event.pattern_match.group(1)
@@ -173,12 +172,11 @@ async def _(event):
         msg = await event.get_reply_message()
         if msg == None:
             return await event.respond(
-                "ɪ ᴄᴀɴ'ᴛ ᴍᴇɴᴛɪᴏɴ ᴍᴇᴍʙᴇʀs ꜰᴏʀ ᴏʟᴅᴇʀ ᴍᴇssᴀɢᴇs! (ᴍᴇssᴀɢᴇs ᴡʜɪᴄʜ ᴀʀᴇ sᴇɴᴛ ʙᴇꜰᴏʀᴇ ɪ'ᴍ ᴀᴅᴅᴇᴅ ᴛᴏ ɢʀᴏᴜᴘ)"
+                "ᴀᴋᴜ ᴛɪᴅᴀᴋ ᴅᴀᴘᴀᴛ ᴍᴇɴɢᴍᴇɴᴛɪᴏɴ ᴍᴇᴍʙᴇʀs ʙᴇʀᴅᴀsᴀʀᴋᴀɴ ᴘᴇsᴀɴ ʏᴀɴɢ ᴅɪᴋɪʀɪᴍ ᴘᴀᴅᴀ ᴍᴀsᴀʟᴀʜ ᴘᴇsᴀɴ ʏᴀɴɢ ᴅɪᴋɪʀɪᴍ ʙᴇꜰᴏʀᴇ ʙᴏᴛ ᴅɪᴛᴀᴍʙᴀʜᴋᴀɴ ᴋᴇ ɢʀᴜᴘ)"
             )
     else:
         return await event.respond(
-            "ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴏʀ ɢɪᴠᴇ ᴍᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ ᴍᴇɴᴛɪᴏɴ ᴏᴛʜᴇʀs!"
-        )
+            "ʙᴀʟᴀsɪ ᴋᴇᴘᴀᴅᴀ ᴍᴇssᴀɢᴇ ᴀᴛᴀᴜ ʙᴇʀɪᴋᴀɴʟᴀʜ ᴘᴀᴅᴀ ᴛᴇᴋs ᴜɴᴛᴜᴋ ᴍᴇɴɢᴍᴇɴᴛɪᴏɴ ʟᴀɪɴ!"
 
     spam_chats.append(chat_id)
     usrnum = 0
@@ -198,22 +196,21 @@ async def _(event):
             await asyncio.sleep(2)
             usrnum = 0
             usrtxt = ""
-    try:
-        spam_chats.remove(chat_id)
-    except:
-        pass
-
+try:
+    spam_chats.remove(chat_id)
+except:
+    pass
 
 @client.on(events.NewMessage(pattern="^/cancel$"))
 async def cancel_spam(event):
     if not event.chat_id in spam_chats:
-        return await event.respond("ᴛʜᴇʀᴇ ɪs ɴᴏ ᴘʀᴏᴄᴄᴇss ᴏɴ ɢᴏɪɴɢ...")
+        return await event.respond("ᴛɪᴅᴀᴋ ᴀᴅᴀ ᴘʀᴏsᴇs ʏᴀɴɢ ʙᴇʀᴊᴀʟᴀɴ...")
     else:
         try:
             spam_chats.remove(event.chat_id)
         except:
             pass
-        return await event.respond("sᴛᴏᴘᴘᴇᴅ.")
+        return await event.respond("ᴅɪʜᴇɴᴛɪᴋᴀɴ.")
 
 
 print(">> ASAD ALEXA WORKING <<")
